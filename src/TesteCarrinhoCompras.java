b9e3bea8fb78fbb9285b11d5d2920cd35eb2337a
Luan Andrade <luansandrade92@hotmail.com>
2016-07-18 17:07:36 -0300

    inicio criação mock

--- src/TesteCarrinhoCompras.java
+++ src/TesteCarrinhoCompras.java
@@ -13,5 +13,14 @@ public class TesteCarrinhoCompras {
 		c.adicionaProduto(new Produto("bermuda",70));
 		assertEquals(220, c.total());
 	}
+	
+	@Test
+	public void escutaAdicaoDeProduto(){
+		CarrinhoCompras c = new CarrinhoCompras();
+		MockObservadorCarrinho mock = new MockObservadorCarrinho();
+		c.adicionarObservador(mock);
+		c.adicionaProduto(new Produto("bermuda",70));
+		mock.verificaRecebimentoProduto("bermuda",70);
+	}
 
 }
